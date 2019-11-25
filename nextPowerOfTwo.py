24aedc9b2950bae368d98933f8a8b4f910c439d6
SethBRichards <25270821+SethBRichards@users.noreply.github.com>
2019-11-24 21:01:53 -0800

    Update nextPowerOfTwo.py

--- nextPowerOfTwo.py
+++ nextPowerOfTwo.py
@@ -2,12 +2,16 @@ import math
 
 def nextPowerOfTwo(n):
     # next_power_of_2(n) = n
-    # This function rounds value of n up to the nearest higher power of 2
+    # This function rounds value of n up to the nearest higher power of 2, and returns that power
     #
     # Inputs:
-    # Any single number, no arrays, no negatives, <<
+    # Any single number, no arrays, no negatives
+    #
+    # Outputs:
+    # The next power of 2 from n (returns the power not the number
     #
     # See: https://www.geeksforgeeks.org/smallest-power-of-2-greater-than-or-equal-to-n/
+    # Built to mirror MATLAB function nextPow2()
     #
     # Author: Seth Richards
     # Version Date 2019/11/11
@@ -18,5 +22,6 @@ def nextPowerOfTwo(n):
         return 1
     else:
         n = 2 ** (math.ceil(math.log(n,2)))
+        n = math.log(n,2)
     return n
 
