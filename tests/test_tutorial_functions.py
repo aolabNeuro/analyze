96629b9f894ae9aaaea619431ddf39be8dc02cca
m-nolan <manolan@uw.edu>
2021-03-30 16:13:18 -0700

    Add: test added for tutorial_functions.py:practice_func_miken, passing.

--- tests/test_tutorial_functions.py
+++ tests/test_tutorial_functions.py
@@ -8,5 +8,13 @@ class TutorialTests(unittest.TestCase):
         self.assertEqual(type(output), str)
         self.assertEqual(output, 'You have 3 fish')
 
+    def test_miken_practice(self):
+        best_num = 7
+        not_best_num = 8
+        best_result = aopy.tutorial_functions.practice_func_miken(best_num)
+        not_best_result = aopy.tutorial_functions.practice_func_miken(not_best_num)
+        self.assertTrue(best_result)
+        self.assertTrue(~not_best_result)
+
 if __name__ == "__main__":
     unittest.main()
