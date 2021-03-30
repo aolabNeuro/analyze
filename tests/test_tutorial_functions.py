8f794cccf0afb625152f7509b6beac44c9b4e5aa
Pamel Kang <pamelk2@uw.edu>
2021-03-30 16:56:12 -0700

    Pamel's github practice

--- tests/test_tutorial_functions.py
+++ tests/test_tutorial_functions.py
@@ -8,5 +8,11 @@ class TutorialTests(unittest.TestCase):
         self.assertEqual(type(output), str)
         self.assertEqual(output, 'You have 3 fish')
 
+
+    def pamels_function(self):
+        output = aopy.tutorial_functions.practice_func_leo(10)
+        self.assertEqual(type(output), str)
+        self.assertEqual(output, 'Pamel has 10 cookies. She will eat all of them')
+
 if __name__ == "__main__":
     unittest.main()
