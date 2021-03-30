7fad2d7b8c6c03ee7c66dcc6ee07f64fc3366cb9
zaicheek <gusmv@uw.edu>
2021-03-30 16:49:42 -0700

    gus' second attempt at tutorial

--- tests/test_tutorial_functions.py
+++ tests/test_tutorial_functions.py
@@ -8,5 +8,10 @@ class TutorialTests(unittest.TestCase):
         self.assertEqual(type(output), str)
         self.assertEqual(output, 'You have 3 fish')
 
+    def test_gus_practice(self):
+        output = aopy.tutorial_functions.practice_function_gus('chicken')
+        self.assertEqual(type(output), str)
+        self.assertEqual(output, 'my favorite food is chicken')
+
 if __name__ == "__main__":
     unittest.main()
