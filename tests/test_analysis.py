4f4af0be6d75cacdd5cfe1adf9ed3bfec0b26eb1
Ryan Canfield <rcanfiel@uw.edu>
2021-04-12 21:40:13 -0700

    Ryan add kfold factor analysis: Add function kfold_factor_analysis to analysis.py address comments

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -29,10 +29,16 @@ class factor_analysis_tests(unittest.TestCase):
         test_data[:,15] = 2+ factor1 - .136*factor2 + np.random.normal(0, 1, size = (n))
 
 
-        ncomp_fa = np.arange(0,5 ,1).astype(int)
-        log_likelihood_score, iterations_required = aopy.analysis.kfold_factor_analysis(test_data, ncomp_fa, nfold= 3, maxiter = 100, verbose = True)
-        data_dimensionality = np.argmax(np.mean(log_likelihood_score, 1))
+        dimensions = np.arange(0,5 ,1).astype(int)
 
+        # Test for no cross validation (nfold = 1)
+        log_likelihood_score_1, iterations_required_1 = aopy.analysis.factor_analysis_dimensionality_score(test_data, dimensions, nfold=1, maxiter=1000, verbose=True)
+        data_dimensionality_1 = np.argmax(np.mean(log_likelihood_score_1, 1))
+        self.assertEqual(data_dimensionality_1, 4) # With no cross validation, the model will always best to the highest dimension tested
+
+        # Test for nfold > 1
+        log_likelihood_score, iterations_required = aopy.analysis.factor_analysis_dimensionality_score(test_data, dimensions, nfold=3, maxiter=1000, verbose=True)
+        data_dimensionality = np.argmax(np.mean(log_likelihood_score, 1))
         self.assertEqual(data_dimensionality, 2)
 
 
