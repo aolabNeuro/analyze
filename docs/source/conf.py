dbd7c6accce82687abd2709acd55f72272e1da5b
rcanfield2 <46601584+rcanfield2@users.noreply.github.com>
2021-04-30 00:05:34 -0700

    readthedocs_debug2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
-
+autodoc_mock_imports = ['_tkinter']
 
 # -- Project information -----------------------------------------------------
 
@@ -53,4 +53,4 @@ exclude_patterns = []
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
