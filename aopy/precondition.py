5b53aa5747ab1174a208a40fa3b9414e48abf236
pavi-rajes <pavir@uw.edu>
2021-05-27 15:10:12 -0700

    multitaper analysis

--- aopy/precondition.py
+++ aopy/precondition.py
@@ -4,6 +4,42 @@ from scipy import signal
 from scipy.signal import butter, lfilter, filtfilt, windows
 import math
 import numpy as np
+import os
+
+# TODO: Fix issues with installing nitime
+import nitime.algorithms as tsa
+import nitime.utils as utils
+from nitime.viz import winspect
+from nitime.viz import plot_spectral_estimate
+'''
+Filter functions
+'''
+def generate_test_signal(T, fs, freq, a=0.02):
+    '''
+
+    Args:
+        T (float): Time period in seconds
+        fs (int): Sampling frequency in Hz
+        freq (1D array): Frequencies to be mixed in the test signal. main frequency in the first element
+        plot_true (bool): boolean value if true, plots the test signal in the provided axes
+        ax (plot axis handle): axis to plot the noisy test signal
+
+    Returns:
+        x (1D array): cosine wave with multiple frequencies and noise
+    '''
+    nsamples = int(T * fs)
+    t = np.linspace(0, T, nsamples, endpoint=False)
+    # a = 0.02
+    f0 = freq[0]
+    # noise_power = 0.001 * fs / 2
+
+    x = a * np.cos(2 * np.pi * f0 * t)  # adding primary frequency
+    x += a * 0.5 * np.cos(2 * np.pi * freq[1] * t)
+    x += a * 1.5 * np.cos(2 * np.pi * freq[2] * t)
+    x += a * 20 * np.sin(2 * np.pi * 1.2 * np.sqrt(t))  # noise
+    # x += np.random.normal(scale=np.sqrt(noise_power), size=t.shape)  # noise
+
+    return x, t, f0
 
 def bandpass_butterworth_params(cutoff_low, cutoff_high, fs, order = 4, filter_type = 'bandpass'):
     '''
@@ -14,7 +50,8 @@ def bandpass_butterworth_params(cutoff_low, cutoff_high, fs, order = 4, filter_t
         order (int): Order of the butter worth filter
         filter_type (str) : Type of filter. Accepts one of the four values - {‘lowpass’, ‘highpass’, ‘bandpass’, ‘bandstop’}
 
-    Returns: bandpass filter parameters
+    Returns:
+        bandpass filter parameters
 
     '''
     nyq = 0.5 * fs
@@ -28,8 +65,8 @@ def bandpass_butterworth_filter_data(data, cutoff_low, cutoff_high, fs, order = 
     '''
     Args:
         data (array): neural data
-        cutoff_low (int): lower cut-off frequency (in Hz)
-        cutoff_high (int): higher cutoff frequency (in Hz)
+        cutoff_low (float): lower cut-off frequency (in Hz)
+        cutoff_high (float): higher cutoff frequency (in Hz)
         fs (int): sampling rate (in Hz)
         order: Order of the butterworth filter
         filter_type (str) : Type of filter. Accepts one of the four values - {‘lowpass’, ‘highpass’, ‘bandpass’, ‘bandstop’}
@@ -44,93 +81,101 @@ def bandpass_butterworth_filter_data(data, cutoff_low, cutoff_high, fs, order = 
     filtered_data = filtfilt(b, a, data)
     return filtered_data
 
-def bandpass_multitaper_filter_data(data, cutoff_low, cuttoff_high, fs, window_len = 50, std_half_bw=3):
+def bandpass_multitaper_filter_data(data, fs, NW = None, BW= None, adaptive = False, jackknife = True, sides = 'default'):
     '''
 
     Args:
-        data (numpy array) : Time series neural data - n_timepoints x n_channels
-        cutoff_low (int): lower cutoff frequency (in Hz)
-        cuttoff_high (int): higher cutoff frequency (in Hz)
-        fs (int): sampling rate (in Hz)
-        window_len (int): W - choice of W decides how much to smooth in the multitaper filter
-        std_half_bw (float): standardised half bandwidth - bandwidth o frequency smoothing in Hz
+        data (ndarray):  time series data where time axis is assumed to be on the last axis
+        fs (float): sampling rate of the signal
+        NW (float): Normalized half bandwidth of the data tapers in Hz
+        BW (float): sampling bandwidth of the data tapers in Hz
+        adaptive (bool): Use an adaptive weighting routine to combine the PSD estimates of different tapers.
+        jackknife (bool): Use the jackknife method to make an estimate of the PSD variance at each point.
+        sides (str): This determines which sides of the spectrum to return.
 
     Returns:
-
-
+        f (ndarray) : Frequency points vector
+        psd_est (ndarray): estimated power spectral density (PSD)
+        nu (ndarray): if jackknife = True; estimated variance of the log-psd. If Jackknife = False; degrees of freedom in a chi square model of how the estimated psd is distributed wrt true log - PSD
     '''
+    f, psd_mt, nu = tsa.multi_taper_psd(data, fs, NW, BW,  adaptive, jackknife, sides )
+    return f, psd_mt, nu
 
-    nt = data.shape[0]  # calculate the number of points
-    # nch = data.shape[1]  # calculate the number of channels
-    n = nt // fs // 10 # array n equals array nt divided by 10 times sampling - default window size
-    dn = np.true_divide(n, 10)
-    k = math.floor(2*window_len * std_half_bw -1)
-    # tapers, taper_weight = windows.dpss(window_len, std_half_bw, Kmax=k,  sym= True)
-#
-#     n_tapers = tapers.shape[0]
-#     n_timepoints = tapers.shape[1]
+#TODO: function for LFP band power
 
-#     fk = [0, np.true_divide(fs, 2.)]
-#
-#     dn = math.floor(dn * fs)
-#     pad = 2 # padding factor for fft
-#
-#     np2 =  2 ** (math.ceil(math.log(n_timepoints+1,2)))
-#     np2 = math.log(n_timepoints+1,2)
-#     nf = np.maximum(256, pad * 2 ** nextPowerOfTwo(np2))
-#     # temp = np.multiply(sampling, nf)
-#     # fk = np.true_divide(fk,sampling)
-#     nfk = np.floor(np.array(fk) * nf / fs)
-#
-#     nwin = np.floor(np.true_divide((nt - n_timepoints), dn))  # calculate the number of windows
-#     nfr = np.int(np.diff(nfk)[0])
-#     f = np.linspace(fk[0], fk[1], nfr)
-#
-#     for win in range(0, int(nwin)):
-#         # Here the optimized spectral loop starts.
-#         if contflag = 1:
-#             tmp = signal.detrend(X[:,dn*win:(dn*win+N)]).T
-#
-#             if tmp.shape[1] > N :# machine precision work-around? added by alo for weird behavior 181000020
-#                 tmp = tmp[1:N, :]
-#
-#         else:
-#             if nch > 1:
-#                 mX = X[:,dn*win:(dn*win+N)].mean(axis=0)
-#
-# #                     extendedArray = extendArrayWithCurrentData(mX, 0, nch, False)
-#                 tmp = (X[:,dn*win:(dn*win+N)] - mX).T # N x nch
-#             else:
-#                 tmp = X[:,dn*win:(dn*win+N)].T
-#
-#
-#         # this can all be done in a single pass. Don't for-loop.
-#         lowerBound = int(nfk[0])
-#         upperBound = int(nfk[1])
-#         inputArray = np.einsum('ij,ki->ijk',tmp,dpss_tapers) # N x nch x k
-#         Xk = np.fft.fft(inputArray,axis=0,n=int(nf))
-#         Xk = Xk[lowerBound:upperBound,]
-#         XkSquare = (Xk * np.conj(Xk)).real
-#         spec[:,win,:] = XkSquare.mean(axis=-1).T
+def get_psd(data, fs):
+    f, psd = signal.welch(data, fs, average='mean')
+    return f,psd
 
-    slepian_seq = windows.dpss(window_len,std_half_bw, Kmax=k)
+'''
+Plots to test filter performance
+'''
 
-    slepian_data = []
-    n_tapers = slepian_seq.shape[0]
-    n_timepoints = slepian_seq.shape[1]
-    n_win = np.floor(data.shape[0] / window_len)
-    # nws = np.true_divide(n,10)
-    # nws = math.floor(nws*fs)
-    nws = 100
 
-    for i in range(n_tapers):
-        slep_dat = np.zeros_like(data)
-        for win in range(int(n_win)):
-            tmp_data = data[nws*win:nws*win+window_len]
-            slep_dat[nws*win:nws*win+window_len] = tmp_data * slepian_seq[i, :]
-        slepian_data.append(slep_dat)
+def plot_filtered_signal(t,x, x_filter,low, high ):
+    # Plotting noisy test signal and filtered signal
+    plt.figure()
+    plt.clf()
+    plt.plot(t, x, label='Noisy signal')
+    plt.plot(t, x_filter, label='Filtered signal (%g - %gHz)' % low %high)
+    plt.xlabel('time (seconds)')
+    # plt.hlines([-self.a, self.a], 0, self.T, linestyles='--')
+    plt.grid(True)
+    plt.axis('tight')
+    plt.legend(loc='best')
+    plt.show()
 
-    return slepian_seq, slepian_data
+def plot_phase_locking(t, a, f0,  x_filter):
+    # Plotting filtered signal with original signal frequency
+    plt.figure()
+    plt.clf()
+    x_f0 = a * np.cos(2 * np.pi * f0 * t)
+    plt.plot(t, x_f0, label='Original signal (%g Hz)' % f0)
+    plt.plot(t, x_filter, label='Filtered signal (%g Hz)' % f0)
+    plt.xlabel('time (seconds)')
+    plt.ylabel('amplitude')
+    plt.title('Comparison of Original Vs Filtered Signal')
+    plt.legend(loc='best')
+    plt.show()
+
+def plot_freq_response_vs_filter_order(x,lowcut, highvut,fs):
+    # Plot the frequency response for a few different orders
+    plt.figure(2)
+    plt.clf()
+    for order in [2,3,4,5,6]: # trying  different order of butterworth to see the roll off around cut-off frequencies
+        precondition.bandpass_butterworth_filter_data(x, lowcut, highcut, fs, order = order)
+
+        b, a = precondition.bandpass_butterworth_params(lowcut, highcut,fs, order = order)
+        w, h = freqz(b, a, worN=2000)
+        plt.plot((fs * 0.5 / np.pi) * w, abs(h), label="order = %d" % order)
+
+    plt.plot([0, 0.5 * fs], [np.sqrt(0.5), np.sqrt(0.5)], '--', label='sqrt(0.5)')
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Gain')
+    plt.grid(True)
+    plt.legend(loc='best')
+    plt.title('Comparison of Frequency response for Diff. Orders of Butterworth Filter')
+    plt.show()
+
+def plot_psd(x,x_filter, fs):
+    # Plot power spectral density of the signal
+    f, psd = signal.welch(x, fs, average = 'mean')
+    f_filtered, psd_filterred = signal.welch(x_filter, fs, average='median')
+    plt.figure()
+    plt.semilogy(f, psd, label='test signal')
+    plt.semilogy(f_filtered, psd_filterred, label='filtered output')
+    plt.xlabel('frequency [Hz]')
+    plt.ylabel('PSD')
+    plt.legend()
+    plt.title('Power Spectral Density Comparison')
+    plt.show()
+
+def plot_db_spectral_estimate(freq, psd, psd_filter, labels):
+    psd = 10* np.log10(psd)
+    psd_filter = 10 * np.log10(psd_filter)
+    plt.figure()
+    plot_spectral_estimate(freq,psd,(psd_filter,), elabels=(labels))
+    plt.show()
 
 
 
--- tests/test_precondition.py
+++ tests/test_precondition.py
@@ -1,133 +1,70 @@
 # we are generating noisy test data using sine and cosine functions with multiple frequencies
-
-import numpy as np
+import unittest
+from aopy.visualization import *
 import matplotlib.pyplot as plt
-from scipy import signal
-from scipy.signal import freqz
 from aopy import precondition
+import time
 
 
-# # Generating a random noise for testing
-
-# test_freq = []
-def generate_test_signal(T, fs, freq, a= 0.02):
-    '''
-
-    Args:
-        T (float): Time period in seconds
-        fs (int): Sampling frequency in Hz
-        freq (1D array): Frequencies to be mixed in the test signal. main frequency in the first element
-        plot_true (bool): boolean value if true, plots the test signal in the provided axes
-        ax (plot axis handle): axis to plot the noisy test signal
-
-    Returns:
-        x (1D array): cosine wave with multiple frequencies and noise
-    '''
-    nsamples = int(T * fs)
-    t = np.linspace(0, T, nsamples, endpoint=False)
-    # a = 0.02
-    f0 = freq[0]
-    # noise_power = 0.001 * fs / 2
-
-    x = a * np.cos(2 * np.pi * f0 * t) # adding primary frequency
-    x += a*0.5 * np.cos(2 * np.pi * freq[1] * t)
-    x += a*1.5 * np.cos(2 * np.pi * freq[2] * t)
-    x += a*20 * np.sin(2 * np.pi * 1.2 * np.sqrt(t)) # noise
-    # x += np.random.normal(scale=np.sqrt(noise_power), size=t.shape)  # noise
-
-    return x, t, f0
-
-# testing generate test_signal
-T = 0.05
-fs = 25000
-freq = [600, 312, 2000]
-a = 0.02
-x, t, f0= generate_test_signal(T, fs, freq, a)
-
-# Sample rate and desired cutoff frequencies (in Hz).
-# fs = 25000.0
-lowcut = 500.0
-highcut = 1200.0
-y = precondition.bandpass_butterworth_filter_data(x, lowcut, highcut, fs)
-
-# Plotting noisy test signal and filtered signal
-plt.figure()
-plt.clf()
-plt.plot(t, x, label='Noisy signal')
-plt.plot(t, y, label='Filtered signal (%g Hz)' % f0)
-plt.xlabel('time (seconds)')
-plt.hlines([-a, a], 0, T, linestyles='--')
-plt.grid(True)
-plt.axis('tight')
-plt.legend(loc='best')
-plt.show()
-
-# Plotting filtered signal with original signal frequency
-plt.figure(1)
-plt.clf()
-x_f0 = a * np.cos(2 * np.pi * f0 * t)
-plt.plot(t, x_f0, label='Original signal (%g Hz)' % f0)
-plt.plot(t, y, label='Filtered signal (%g Hz)' % f0)
-plt.xlabel('time (seconds)')
-plt.ylabel('amplitude')
-plt.title('Comparison of Original Vs Filtered Signal')
-plt.legend(loc='best')
-plt.show()
-
-# Plot the frequency response for a few different orders
-plt.figure(2)
-plt.clf()
-for order in [2,3,4,5,6]: # trying  different order of butterworth to see the roll off around cut-off frequencies
-    precondition.bandpass_butterworth_filter_data(x, lowcut, highcut, fs, order = order)
-
-    b, a = precondition.bandpass_butterworth_params(lowcut, highcut,fs, order = order)
-    w, h = freqz(b, a, worN=2000)
-    plt.plot((fs * 0.5 / np.pi) * w, abs(h), label="order = %d" % order)
-
-plt.plot([0, 0.5 * fs], [np.sqrt(0.5), np.sqrt(0.5)], '--', label='sqrt(0.5)')
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Gain')
-plt.grid(True)
-plt.legend(loc='best')
-plt.title('Comparison of Frequency response for Diff. Orders of Butterworth Filter')
-plt.show()
-
-# Plot power spectral density of the signal
-
-f, psd = signal.welch(x, fs, average = 'mean')
-f_filtered, psd_filterred = signal.welch(y, fs, average='median')
-plt.figure(3)
-plt.semilogy(f, psd, label='test signal')
-plt.semilogy(f_filtered, psd_filterred, label='filtered output')
-plt.xlabel('frequency [Hz]')
-plt.ylabel('PSD')
-plt.legend()
-plt.title('Power Spectral Density Comparison')
-plt.show()
-
-# Testing Multitaper filter
-window_len = 100
-std_half_bw = 5/window_len
-
-slepian_seq, slepian_data = precondition.bandpass_multitaper_filter_data(x,lowcut,highcut,fs,window_len,std_half_bw)
-
-fig, ax = plt.subplots(8, 2, figsize = (9,7))
-for i in range(8):
-    ax[i,0].plot(slepian_seq[i,:], label='slepian sequence')
-    ax[i,1].plot(slepian_data[i], '-k', label='slepian seq * test data')
-    # ax[i,1].plot(x, '--b', label = 'test data')
-plt.tight_layout()
-plt.suptitle('Slepian Functions in Time Domain')
-plt.show()
+test_dir = os.path.dirname(__file__)
+write_dir = os.path.join(test_dir, 'tmp')
+if not os.path.exists(write_dir):
+    os.mkdir(write_dir)
 
 
-fig, ax = plt.subplots(8, 2, figsize = (9,7))
-for i in range(8):
-    w,h = freqz(slepian_seq[i,:])
-    ax[i, 0].plot(w, 20 * np.log10(np.abs(h)), lw=1.0)
+class FilterTests(unittest.TestCase):
 
-    w2,h2 = freqz(slepian_data[i])
-    ax[i,1].plot(w2, 20*np.log10(np.abs(h2)), '-k', lw =1.0)
-plt.tight_layout()
-plt.suptitle('Slepian Functions in Frequency Domain')
-plt.show()
+    def __init_subclass__(self, **kwargs):
+        self.T = 0.05
+        self.fs = 25000
+        self.freq = [600, 312, 2000]
+        self.a = 0.02
+
+    def test_butterworth(self):
+        # testing generate test_signal
+
+        self.x, self.t, self.f0 = precondition.generate_test_signal(self.T, self.fs, self.freq, self.a)
+
+        # Sample rate and desired cutoff frequencies (in Hz).
+        # fs = 25000.0
+        self.lowcut = 500.0
+        self.highcut = 1200.0
+        tic = time.perf_counter()
+        self.x_filter = precondition.bandpass_butterworth_filter_data(self.x, self.lowcut, self.highcut, self.fs)
+        toc = time.perf_counter()
+        print(f" Butterworth filter executed in {toc - tic:0.4f} seconds")
+
+    def test_plot_filtered_signal(self):
+        # Plotting noisy test signal and filtered signal
+        fname = 'test_signal_filtered_Signal.png'
+        precondition.plot_filtered_signal(self.t, self.x, self.x_filter, self.lowcut, self.highcut)
+        plt.show()
+        savefig(write_dir, fname)
+
+    def test_plot_phase_locking(self):
+        # Plotting filtered signal with original signal frequency
+        fname = 'test_phase_locking.png'
+        precondition.plot_phase_locking(self.t, self.a, self.f0, self.x_filter)
+        plt.show()
+        savefig(write_dir, fname)
+
+    def test_plot_freq_response_vs_filter_order(self):
+        fname = 'freq_response_vs_filter_order.png'
+        precondition.plot_freq_response_vs_filter_order(self.x, self.lowcut, self.highcut, self.fs)
+        plt.show()
+        savefig(write_dir, fname)
+
+    def test_plot_psd(self):
+        fname = 'plot_psd.png'
+        precondition.plot_psd(self.x, self.x_filter, self.fs)
+        plt.show()
+        savefig(write_dir, fname)
+
+    def test_multitaper(self):
+
+        f, psd_filter = precondition.bandpass_multitaper_filter_data(self.x,self.fs)
+        psd = precondition.get_psd(self.x,self.fs)
+        fname = 'multitaper_powerspectrum.png'
+        precondition.plot_db_spectral_estimate(f,psd,psd_filter,'multitaper')
+        plt.show()
+        savefig(write_dir, fname)
