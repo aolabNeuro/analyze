ac64f9ed0a71a9bd3c69287fdcdff298f35d5dbb
pavi <pavir@uw.edu>
2021-07-13 16:34:45 -0700

    docstring update by Nick

--- aopy/utils.py
+++ aopy/utils.py
@@ -36,12 +36,13 @@ def pkl_write(file_to_write, values_to_dump, write_dir):
     '''
     This functions write data into a pickle file
     Args:
-        file_to_write (str): filename
-        values_to_dump (): values to write in a pickle file
-        write_dir: where do you want to write this file
+        file_to_write (str): filename with '.pkl' extension
+        values_to_dump (any): values to write in a pickle file
+        write_dir (str): Path - where do you want to write this file
 
     Returns: None
 
+    examples: pkl_write(meta.pkl, data, '/data_dir')
     '''
     file = os.path.join(write_dir, file_to_write)
     with open(file, 'wb') as pickle_file:
@@ -49,6 +50,15 @@ def pkl_write(file_to_write, values_to_dump, write_dir):
 
 
 def pkl_read(file_to_read, read_dir):
+    '''
+    This function takes in path to a pickle file and returns data as it is stored
+    Args:
+        file_to_read (str): filename with '.pkl' extension
+        read_dir (str): Path to folder where the file is stored
+
+    Returns: data in a format as it is stored
+
+    '''
     file = os.path.join(read_dir, file_to_read)
     this_dat = pickle.load(open(file, "rb"))
     return this_dat
