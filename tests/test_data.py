442122c7026af223918b96d9949a932bbda47b52
Michael Nolan <manolan@uw.edu>
2021-07-13 17:51:23 -0700

    Add: tests for tensor dataset class

--- tests/test_data.py
+++ tests/test_data.py
@@ -287,5 +287,34 @@ class FakeDataTests(unittest.TestCase):
         figname = 'gen_test_signal.png'
         savefig(write_dir, figname)
     
+class DatasetTests(unittest.TestCase):
+
+    def test_tensor_dataset(self):
+        # create tensors + dataset
+        n_batch = 10
+        tensor_1 = torch.randn(n_batch,10,5)
+        tensor_2 = torch.randn(n_batch,1,15)
+
+        device = 'cpu'
+
+        tensor_dataset = EcogTensorDataset(
+            tensor_1, tensor_2,
+            device=device
+        )
+
+        # test len
+        tensor_dataset_len = len(tensor_dataset)
+        self.assertTrue(tensor_dataset_len == n_batch)
+
+        # test sampling
+        sample_idx = 0
+        sample = next(iter(tensor_dataset))
+        self.assertTrue(
+            sample[0] == tensor_1[sample_idx,:,:] and sample[1] == tensor_2[sample_idx,:,:]
+        )
+
 if __name__ == "__main__":
     unittest.main()
+
+
+
