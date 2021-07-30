eac8e54e9929bc5f3321ce5f4a4ac8d84c6cfbf9
pavi-rajes <pavir@uw.edu>
2021-07-29 18:08:15 -0700

    Updated documentation for KF functions

--- aopy/analysis.py
+++ aopy/analysis.py
@@ -74,30 +74,73 @@ Curve fitting
 
 # These functions are for curve fitting and getting modulation depth and preferred direction from firing rates
 def func(target, b1, b2, b3):
+    '''
+
+    Args:
+        target (int) center out task target index ( takes values from 0 to 7)
+        b1, b2, b3 : parameters used for curve fitting
+
+    .. math::
+    
+        b1 * cos(\\theta) + b2 * sin(\\theta) + b3
+
+    Returns: result from above equation
+
+    '''
     theta = 45 * (target - 1)
     return b1 * np.cos(np.deg2rad(theta)) + b2 * np.sin(np.deg2rad(theta)) + b3
 
 
 def get_modulation_depth(b1, b2):
+    '''
+    Calculates modulation depth from curve fitting parameters as follows:
+    
+    .. math::
+    
+        \\sqrt{b_1^2+b_2^2}
+
+    '''
     return np.sqrt((b1 ** 2) + (b2 ** 2))
 
 
 def get_preferred_direction(b1, b2):
+    '''
+    Calculates preferred direction from curve fitting parameters as follows:
+    
+    .. math:: 
+        
+        arctan(\\frac{b_1^2}{b_2^2})
+        
+    '''
     return np.arctan2(b2 ** 2, b1 ** 2)
 
 
-def get_mean_fr_per_direction(data, target):
+def get_mean_fr_per_direction(data, target_dir):
+    '''
+
+    Args:
+        data (3D array): Neural data in the form of a 3D array neurons X trials x timebins
+        target_dir (1D array): target direction
+
+    Returns:
+        means_d = mean firing rate per neuron per target direction
+        stds_d = standard deviation from mean firing rate per neuron
+    '''
     means_d = []
     stds_d = []
 
     for i in range(1, 9):
-        means_d.append(np.mean(data[target == i], axis=(0, 2)))
-        stds_d.append(np.std(data[target == i], axis=(0, 2)))
+        means_d.append(np.mean(data[target_dir == i], axis=(0, 2)))
+        stds_d.append(np.std(data[target_dir == i], axis=(0, 2)))
 
     return means_d, stds_d
 
 
 def plot_mean_fr_per_target_direction(means_d, neuron_id, ax, color, this_alpha, this_label):
+    '''
+    generate a plot of mean firing rate per target direction
+
+    '''
     sns.set_context('talk')
     ax.plot(np.array(means_d)[:, neuron_id], c=color, alpha=this_alpha, label=this_label)
 
@@ -111,15 +154,15 @@ def run_curvefitting(means, make_plot=True, fig_title='Tuning Curve', n_subplot_
     '''
     Args:
         means (2D array) : Mean firing rate [n_targets x n_neurons]
-        make_plot (bool) : Generates plot with curve fitting and mean firing rate for n_neuons 
-        Fig title (str) : Figure title 
+        make_plot (bool) : Generates plot with curve fitting and mean firing rate for n_neuons
+        Fig title (str) : Figure title
         n_rows (int) : No of rows in subplot
         n_cols (int) : No. of cols in subplot
-        
-    Returns: 
-        params_day (Numpy array) : Curve fitting parameters 
+
+    Returns:
+        params_day (Numpy array) : Curve fitting parameters
         modulation depth (Numpy array) : Modulation depth of neuron
-        preferred direction (Numpy array) : preferred direction of neurons 
+        preferred direction (Numpy array) : preferred direction of neurons
     '''
     params_day = []
     mod_depth = []
@@ -150,7 +193,7 @@ def run_curvefitting(means, make_plot=True, fig_title='Tuning Curve', n_subplot_
             plt.xticks(np.arange(1, 8, 2))
     return np.array(params_day), np.array(mod_depth), np.array(pd)
 
-  
+
 def find_trough_peak_idx(unit_data):
     '''
     This function calculates the trough-to-peak time at the index level (0th order) by finding the minimum value of
@@ -249,13 +292,34 @@ def interpolate_extremum_poly2(extremum_idx, data, extrap_peaks=False):
 
     return extremum_time, extremum_value, f
 
+def get_fano_factor_values_per_condition(spiking_data):
+    '''
+    This function calculates the two parameters used to calculate fano factor for input spiking data based on Churchland et al. 2010.
+    These two parameters are calculated for each unit and are the mean spikes per trial and the spiking variance of each unit across trials.
+
+    Args:
+        spiking_data (ntime, nunits, ntr): Input spiking data
+        weight_regression (bool): A flag to weight the linear regression based on the number of trials.
+
+    Returns:
+        Tuple:  A tuple containing
+            unit_mean: The mean spike counts for each unit across the input time
+            unit_variance: The spike count variance for each unit across the input time
+    '''
+
+    counts = np.sum(spiking_data, axis=1) # Counts has the shape (nunits, ntr)
+    unit_mean = np.mean(counts, axis=1) # Averge the counts for each unit across all trials
+    unit_variance = np.var(counts, axis=1) # Calculate the count variance for each unit across all trials
+
+    return unit_mean, unit_variance
+
 
 '''
 KALMAN FILTER 
 '''
 
-class KFDecoder(object):
 
+class KFDecoder(object):
     """
     Class for the Kalman Filter Decoder
     Parameters
@@ -268,12 +332,10 @@ class KFDecoder(object):
     The original implementation has previously been coded in Matlab by Dan Morris (http://dmorris.net/projects/neural_decoding.html#code)
     """
 
-    def __init__(self,C=1):
-        self.C=C
+    def __init__(self, C=1):
+        self.C = C
 
-
-    def fit(self,X_kf_train,y_train):
-
+    def fit(self, X_kf_train, y_train):
         """
         Train Kalman Filter Decoder
         Parameters
@@ -283,91 +345,91 @@ class KFDecoder(object):
             See example file for an example of how to format the neural data correctly
         y_train (2D numpy array): [n_samples(i.e. timebins), n_outputs]
             This is the outputs that are being predicted
-        
-        Calculations as follows:
-        A = X2*X1' (X1*X1')^-1
-        W = (X2 - A*X1)(X2 - A*X1)' / (# time points - 1)
-        H = Y*X'(X*X')^-1
-        Q = (Y-CX)(Y-CX)' / (# time points)
+
+        Calculations for A,W,H,Q are as follows:
+        .. math:: A = X2*X1' (X1*X1')^{-1}
+        .. math:: W = \frac{(X_2 - A*X_1)(X_2 - A*X_1)'}{(timepoints - 1)}
+        .. math:: H = Y*X'(X*X')^{-1}
+        .. math:: Q = \frac{(Y-HX)(Y-HX)' }{time points}
         """
 
-        #First we'll rename and reformat the variables to be in a more standard kalman filter nomenclature (I am following notations from Wu et al, 2003):
-        #xs are the state (here, the variable we're predicting, i.e. y_train)
-        #zs are the observed variable (neural data here, i.e. X_kf_train)
-        X=np.matrix(y_train.T)
-        Z=np.matrix(X_kf_train.T)
+        # Renaming and reformatting variables to be in a more standard kalman filter nomenclature (from Wu et al, 2003):
+        # xs are the state (here, the variable we're predicting, i.e. y_train)
+        # zs are the observed variable (neural data here, i.e. X_kf_train)
+        X = np.matrix(y_train.T)
+        Z = np.matrix(X_kf_train.T)
 
-        #number of time bins
-        nt=X.shape[1]
+        # number of time bins
+        nt = X.shape[1]
 
-        #Calculate the transition matrix (from x_t to x_t+1) using least-squares, and compute its covariance
-        #In our case, this is the transition from one kinematic state to the next
-        X2 = X[:,1:]
-        X1 = X[:,0:nt-1]
-        
-        A=X2*X1.T*inv(X1*X1.T) #Transition matrix  
-        W=(X2-A*X1)*(X2-A*X1).T/(nt-1)/self.C #Covariance of transition matrix. Note we divide by nt-1 since only nt-1 points were used in the computation (that's the length of X1 and X2). We also introduce the extra parameter C here.
+        # Calculate the transition matrix (from x_t to x_t+1) using least-squares, and compute its covariance
+        # In our case, this is the transition from one kinematic state to the next
+        X2 = X[:, 1:]
+        X1 = X[:, 0:nt - 1]
 
-        #Calculate the measurement matrix (from x_t to z_t) using least-squares, and compute its covariance
-        #In our case, this is the transformation from kinematics to spikes
-        H = Z*X.T*(inv(X*X.T)) #Measurement matrix
-        Q = ((Z - H*X)*((Z - H*X).T)) / nt #Covariance of measurement matrix
-        
-        params=[A,W,H,Q]
-        print('Shape of State Transition model (A) :'+ str(A.shape))
-        print('Shape of Covariance of State Transition model :'+ str(W.shape))
+        A = X2 * X1.T * inv(X1 * X1.T)  # Transition matrix
+        W = (X2 - A * X1) * (X2 - A * X1).T / (
+                    nt - 1) / self.C  # Covariance of transition matrix. Note we divide by nt-1 since only nt-1 points were used in the computation (that's the length of X1 and X2). We also introduce the extra parameter C here.
+
+        # Calculate the measurement matrix (from x_t to z_t) using least-squares, and compute its covariance
+        # In our case, this is the transformation from kinematics to spikes
+        H = Z * X.T * (inv(X * X.T))  # Measurement matrix
+        Q = ((Z - H * X) * ((Z - H * X).T)) / nt  # Covariance of measurement matrix
+
+        params = [A, W, H, Q]
+        print('Shape of State Transition model (A) :' + str(A.shape))
+        print('Shape of Covariance of State Transition model :' + str(W.shape))
         print('Shape of Observation model (H) :' + str(H.shape))
-        print('Shape of Covariance of Observation model :'+ str(Q.shape))
-        self.model=params
+        print('Shape of Covariance of Observation model :' + str(Q.shape))
+        self.model = params
 
     def fit_awf(self, X_kf_train, y_train, A, W):
-      """
-      Train Kalman Filter Decoder with A and W fixed. A is the state transition model and W is the associated covariance 
+        """
+        Train Kalman Filter Decoder with A and W fixed. A is the state transition model and W is the associated covariance
 
-      Parameters
-      ----------
-      X_kf_train (2D numpy array): [n_samples(i.e. timebins) , n_neurons]
-          This is the neural data in Kalman filter format.
-          See example file for an example of how to format the neural data correctly
-      y_train (2D numpy array): [n_samples(i.e. timebins), n_outputs]
-          This is the outputs that are being predicted
-      
-      Calculations as follows:
-      H = Y*X'(X*X')^-1
-      Q = (Y-CX)(Y-CX)' / (# time points)
-      """
+        Parameters
+        ----------
+        X_kf_train (2D numpy array): [n_samples(i.e. timebins) , n_neurons]
+            This is the neural data in Kalman filter format.
+            See example file for an example of how to format the neural data correctly
+        y_train (2D numpy array): [n_samples(i.e. timebins), n_outputs]
+            This is the outputs that are being predicted
 
-      #First we'll rename and reformat the variables to be in a more standard kalman filter nomenclature (following Wu et al, 2003):
-      #xs are the state (here, the variable we're predicting, i.e. y_train)
-      #zs are the observed variable (neural data here, i.e. X_kf_train)
-      X=np.matrix(y_train.T)
-      Z=np.matrix(X_kf_train.T)
+        Calculations as follows:
+        .. math:: H = Y*X'(X*X')^{-1}
+        .. math:: Q = \frac{(Y-HX)(Y-HX)' }{time points}
+        """
 
-      #number of time bins
-      nt=X.shape[1]
+        # Renaming and reformatting variables to be in a more standard kalman filter nomenclature (from Wu et al, 2003):
+        # xs are the state (here, the variable we're predicting, i.e. y_train)
+        # zs are the observed variable (neural data here, i.e. X_kf_train)
+        X = np.matrix(y_train.T)
+        Z = np.matrix(X_kf_train.T)
 
-      #Calculate the transition matrix (from x_t to x_t+1) using least-squares, and compute its covariance
-      #In our case, this is the transition from one kinematic state to the next
-      X2 = X[:,1:]
-      X1 = X[:,0:nt-1]
-      
-      # A=X2*X1.T*inv(X1*X1.T) #Transition matrix  
-      # W=(X2-A*X1)*(X2-A*X1).T/(nt-1)/self.C #Covariance of transition matrix. Note we divide by nt-1 since only nt-1 points were used in the computation (that's the length of X1 and X2). We also introduce the extra parameter C here.
+        # number of time bins
+        nt = X.shape[1]
 
-      #Calculate the measurement matrix (from x_t to z_t) using least-squares, and compute its covariance
-      #In our case, this is the transformation from kinematics to spikes
-      H = Z*X.T*(inv(X*X.T)) #Measurement matrix
-      Q = ((Z - H*X)*((Z - H*X).T)) / nt #Covariance of measurement matrix
+        # Calculate the transition matrix (from x_t to x_t+1) using least-squares, and compute its covariance
+        # In our case, this is the transition from one kinematic state to the next
+        X2 = X[:, 1:]
+        X1 = X[:, 0:nt - 1]
 
-      print('Shape of State Transition model (A) :'+ str(A.shape))
-      print('Shape of Covariance of State Transition model :'+ str(W.shape))
-      print('Shape of Observation model (H) :' + str(H.shape))
-      print('Shape of Covariance of Observation model :'+ str(Q.shape))
-      params=[A,W,H,Q]
-      self.model=params
+        # A=X2*X1.T*inv(X1*X1.T) #Transition matrix
+        # W=(X2-A*X1)*(X2-A*X1).T/(nt-1)/self.C #Covariance of transition matrix. Note we divide by nt-1 since only nt-1 points were used in the computation (that's the length of X1 and X2). We also introduce the extra parameter C here.
 
-    def predict(self,X_kf_test,y_test):
+        # Calculate the measurement matrix (from x_t to z_t) using least-squares, and compute its covariance
+        # In our case, this is the transformation from kinematics to spikes
+        H = Z * X.T * (inv(X * X.T))  # Measurement matrix
+        Q = ((Z - H * X) * ((Z - H * X).T)) / nt  # Covariance of measurement matrix
 
+        print('Shape of State Transition model (A) :' + str(A.shape))
+        print('Shape of Covariance of State Transition model :' + str(W.shape))
+        print('Shape of Observation model (H) :' + str(H.shape))
+        print('Shape of Covariance of Observation model :' + str(Q.shape))
+        params = [A, W, H, Q]
+        self.model = params
+
+    def predict(self, X_kf_test, y_test):
         """
         Predict outcomes using trained Kalman Filter Decoder
         Parameters
@@ -384,33 +446,34 @@ class KFDecoder(object):
             The predicted outputs
         """
 
-        #Extract parameters
-        A,W,H,Q=self.model
+        # Extract parameters
+        A, W, H, Q = self.model
 
-        #First we'll rename and reformat the variables to be in a more standard kalman filter nomenclature (following Wu et al, 2003):
-        #xs are the state (here, the variable we're predicting, i.e. y_train)
-        #zs are the observed variable (neural data here, i.e. X_kf_train)
-        X=np.matrix(y_test.T)
-        Z=np.matrix(X_kf_test.T)
+        # First we'll rename and reformat the variables to be in a more standard kalman filter nomenclature (I am following Wu et al):
+        # xs are the state (here, the variable we're predicting, i.e. y_train)
+        # zs are the observed variable (neural data here, i.e. X_kf_train)
+        X = np.matrix(y_test.T)
+        Z = np.matrix(X_kf_test.T)
 
-        #Initializations
-        num_states=X.shape[0] #Dimensionality of the state
-        states=np.empty(X.shape) #Keep track of states over time (states is what will be returned as y_test_predicted)
-        P_m=np.matrix(np.zeros([num_states,num_states]))
-        P=np.matrix(np.zeros([num_states,num_states]))
-        state=X[:,0] #Initial state
-        states[:,0]=np.copy(np.squeeze(state))
+        # Initializations
+        num_states = X.shape[0]  # Dimensionality of the state
+        states = np.empty(
+            X.shape)  # Keep track of states over time (states is what will be returned as y_test_predicted)
+        P_m = np.matrix(np.zeros([num_states, num_states]))  # This is a priori estimate of X covariance
+        P = np.matrix(np.zeros([num_states, num_states]))  # This is a posteriori estimate of X covariance
+        state = X[:, 0]  # Initial state
+        states[:, 0] = np.copy(np.squeeze(state))
 
-        #Get predicted state for every time bin
-        for t in range(X.shape[1]-1):
-            #Do first part of state update - based on transition matrix
-            P_m=A*P*A.T+W
-            state_m=A*state
+        # Get predicted state for every time bin
+        for t in range(X.shape[1] - 1):
+            # Do first part of state update - based on transition matrix
+            P_m = A * P * A.T + W  # a priori estimate of x covariance ( P(k) = A*P(k-1)*A' + W )
+            state_m = A * state  # a priori estimate of x ( X(k|k-1) = A*X(k-1) )
 
-            #Do second part of state update - based on measurement matrix
-            K=P_m*H.T*inv(H*P_m*H.T+Q) #Calculate Kalman gain
-            P=(np.matrix(np.eye(num_states))-K*H)*P_m
-            state=state_m+K*(Z[:,t+1]-H*state_m)
-            states[:,t+1]=np.squeeze(state) #Record state at the timestep
-        y_test_predicted=states.T
+            # Do second part of state update - based on measurement matrix
+            K = P_m * H.T * inv(H * P_m * H.T + Q)  # Calculate Kalman gain ( K = P_ap*H'* inv(H*P_ap*H' + Q) )
+            P = (np.matrix(np.eye(num_states)) - K * H) * P_m  # (a posteriori estimate, P (I - K*H)*P_ap )
+            state = state_m + K * (Z[:,t + 1] - H * state_m)  # compute a posteriori estimate of x (X(k) = X(k|k-1) + K*(Z - H*X(k|k-1))
+            states[:, t + 1] = np.squeeze(state)  # Record state at the timestep
+        y_test_predicted = states.T
         return y_test_predicted
