bce12868dcf6d8afcad19827db71befd343732aa
Leo Scholl <leo.scholl@gmail.com>
2021-09-22 01:18:35 -0700

    update utils

--- aopy/utils.py
+++ aopy/utils.py
@@ -1,10 +1,11 @@
 # utils.py
-# all extra utility functions belong here
+# Any extra utility functions belong here
+# Note: if the function is going to be used for a paper, then it probably belongs elsewhere.
+
 import numpy as np
 import re
 from datetime import datetime
 import os
-import pickle
 
 def generate_test_signal(duration, samplerate, frequencies, amplitudes, noise_amplitude=0.):
     '''
@@ -109,38 +110,3 @@ def count_unique_symbols(files):
     order = np.argsort(-counts)
 
     return unique_symbols[order], counts[order]
-
-def pkl_write(file_to_write, values_to_dump, write_dir):
-    '''
-    This functions write data into a pickle file.
-    
-    Args:
-        file_to_write (str): filename with '.pkl' extension
-        values_to_dump (any): values to write in a pickle file
-        write_dir (str): Path - where do you want to write this file
-
-    Returns:
-        None
-
-    examples: pkl_write(meta.pkl, data, '/data_dir')
-    '''
-    file = os.path.join(write_dir, file_to_write)
-    with open(file, 'wb') as pickle_file:
-        pickle.dump(values_to_dump, pickle_file)
-
-
-def pkl_read(file_to_read, read_dir):
-    '''
-    This function takes in path to a pickle file and returns data as it is stored
-    
-    Args:
-        file_to_read (str): filename with '.pkl' extension
-        read_dir (str): Path to folder where the file is stored
-
-    Returns:
-        data in a format as it is stored
-
-    '''
-    file = os.path.join(read_dir, file_to_read)
-    this_dat = pickle.load(open(file, "rb"))
-    return this_dat
