011d0caef9d087d01f840fc8bbfd05432a69e8b1
rcanfield2 <46601584+rcanfield2@users.noreply.github.com>
2021-10-20 11:22:32 -0700

    Update detect spikes
    Update detect spikes to accommodate negative spikes.

--- aopy/precondition.py
+++ aopy/precondition.py
@@ -186,14 +186,16 @@ def calc_spike_threshold(spike_filt_data, rms_multiplier=3):
     return rms_multiplier*rms_values
 
 
-def detect_spikes(spike_filt_data, samplerate, wf_length=1000, threshold=None):
+def detect_spikes(spike_filt_data, samplerate, above_thresh=True, wf_length=1000, threshold=None):
     '''
     This function calculates spike times based on threshold crossing of the input data and returns the waveforms if 'wf_length' is not None. 
+    If the threshold desired is a negative value (i.e. extracellular recordings) set 'above_thresh' to False.
     Data must exceed the threshold instead of equaling it.
 
     Args:
         spike_filt_data (nt, nch): Time series neural data to detect spikes and extract waveforms from.
         samplerate (float): Sampling rate [Hz]
+        above_thresh (bool): Indicate if spikes should be detected based on going over a threshold or below a threshold.
         threshold (nch): Threshold input data must cross to indicate a spike for each channel. Must have same non time dimensions as spike_filt_data. If set to 'None', this function will call aopy.precondition.calc_spike_threshold to determine adaquate thresholds. 
         wf_length (fload): Length of waveforms to output [us]. Actual length will be rounded up. If set to 'None', waveforms will not be returned.
 
@@ -209,7 +211,10 @@ def detect_spikes(spike_filt_data, samplerate, wf_length=1000, threshold=None):
         threshold = calc_spike_threshold(spike_filt_data)
 
     # Calculate an array of spike times for each channel organized into a list
-    data_above_thresh_mask = spike_filt_data > threshold
+    if above_thresh:
+        data_above_thresh_mask = spike_filt_data > threshold
+    else:
+        data_above_thresh_mask = spike_filt_data < -threshold
 
     spike_times = []
     spike_waveforms =[]
