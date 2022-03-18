94030e2f5c5017f71bb484ed76da45bc56d281af
Leo Scholl <leo.scholl@gmail.com>
2022-03-18 09:52:19 -0700

    add util

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -353,5 +353,10 @@ class OtherPlottingTests(unittest.TestCase):
         filename = 'events_time'
         savefig(write_dir,filename)
 
+    def test_advance_plot_color(self):
+        # Nothing to test here ;-)
+        pass
+
+
 if __name__ == "__main__":
     unittest.main()
