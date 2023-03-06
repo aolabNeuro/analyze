67f80fb18f9701d290352c6878034506848d7cda
Leo Scholl <leo.scholl@gmail.com>
2023-03-06 08:49:11 -0800

    change float to int

--- aopy/precondition/base.py
+++ aopy/precondition/base.py
@@ -549,18 +549,20 @@ def downsample(data, old_samplerate, new_samplerate):
 
     Args:
         data (nt, ...): timeseries data to be downsampled. Can be 1D or 2D.
-        old_samplerate (float): the current sampling rate of the data
-        new_samplerate (float): the desired sampling rate of the downsampled data
+        old_samplerate (int): the current sampling rate of the data
+        new_samplerate (int): the desired sampling rate of the downsampled data
         
     Returns:
         (nt, ...) downsampled data
     '''
-    assert new_samplerate < old_samplerate
-    assert data.ndim < 3 # doesn't work for more than 2 dimensions
+    assert new_samplerate < old_samplerate, "New sampling rate must be less than old sampling rate"
+    assert data.ndim < 3, "Downsample doesn't work for more than 2 dimensions"
+    assert int(old_samplerate) == old_samplerate, "Input samplerates must be integers"
+    assert int(new_samplerate) == new_samplerate, "Input samplerates must be integers"
 
     # Check if the downsample factor will be an integer, otherwise we find a common divisor
     if old_samplerate % new_samplerate != 0:
-        lcm = np.lcm(old_samplerate, new_samplerate) # least common multiple
+        lcm = np.lcm(int(old_samplerate), int(new_samplerate)) # least common multiple
         print(f"Upsampling first to {lcm} Hz")
         upsampled = np.repeat(data, lcm/old_samplerate, axis=0)
         return downsample(upsampled, lcm, new_samplerate)
