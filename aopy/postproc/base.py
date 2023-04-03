0346a3a60082c6d95cdccfc8e7143f0c815945bf
Leo Scholl <leo.scholl@gmail.com>
2023-04-03 15:38:08 -0700

    speed up rotate data and nudge in all axes

--- aopy/postproc/base.py
+++ aopy/postproc/base.py
@@ -45,34 +45,34 @@ def rotate_spatial_data(spatial_data, new_axis, current_axis):
 
     # Check if input data is a single point and enfore that it is a row vector
     if len(spatial_data.shape) == 1:
-      spatial_data.shape = (1,len(spatial_data))    
+        spatial_data.shape = (1,len(spatial_data))    
 
     # Initialize output array
     output_spatial_data = np.empty((spatial_data.shape[0], 3))
 
     # Check for a 2D or 3D trajectory and convert to 3D points
     if spatial_data.shape[1] == 2:
-      spatial_data3d = np.concatenate((spatial_data, np.zeros((spatial_data.shape[0],1))), axis = 1)
-      new_axis3d = np.concatenate((new_axis, np.array([0])))
-      current_axis3d = np.concatenate((current_axis, np.array([0])))
+        spatial_data3d = np.concatenate((spatial_data, np.zeros((spatial_data.shape[0],1))), axis = 1)
+        new_axis3d = np.concatenate((new_axis, np.array([0])))
+        current_axis3d = np.concatenate((current_axis, np.array([0])))
     elif spatial_data.shape[1] == 3:
-      spatial_data3d = spatial_data
-      new_axis3d = new_axis
-      current_axis3d = current_axis
+        spatial_data3d = spatial_data
+        new_axis3d = new_axis
+        current_axis3d = current_axis
 
     # Calcualte angle between 'new_axis3d' and target trajectory via dot product
     angle = np.arccos(np.dot(new_axis3d, current_axis3d)/(np.linalg.norm(new_axis3d)*np.linalg.norm(current_axis3d)))
 
     # If angle is 0, return the original data and warn
     if np.isclose(angle, 0, atol = 1e-8):
-      warnings.warn("Starting and desired vector are the same. No rotation applied")
-      output_spatial_data = spatial_data3d
-      return output_spatial_data
+        warnings.warn("Starting and desired vector are the same. No rotation applied")
+        output_spatial_data = spatial_data3d
+        return output_spatial_data
 
     # If the angle is exactly 180 degrees, slightly nudge the starting vector by 1e-7
     elif np.isclose(angle, np.pi, atol = 1e-8):
-      current_axis3d = current_axis3d.astype('float64')
-      current_axis3d[0] += 1e-7
+        current_axis3d = current_axis3d.astype('float64')
+        current_axis3d += 1e-7
 
     # Calculate unit vector axis to rotate about via cross product
     rotation_axis = np.cross(current_axis3d, new_axis3d)
@@ -92,14 +92,14 @@ def rotate_spatial_data(spatial_data, new_axis, current_axis):
                                 [2*(qi*qk - qj*qr), 2*(qj*qk + qi*qr), 1-2*(qi**2 + qj**2)]])
 
     # Apply rotation matrix to each point in the trajectory
-    for point_idx in range(spatial_data3d.shape[0]):
-      output_spatial_data[point_idx,:] = rotation_matrix @ spatial_data3d[point_idx,:]
+    output_spatial_data = rotation_matrix @ spatial_data3d.T
+    output_spatial_data = output_spatial_data.T
 
     # Return trajectories the the same dimensions as the input
     if spatial_data.shape[1] == 2:
-      return output_spatial_data[:,:2]
+        return output_spatial_data[:,:2]
     elif spatial_data.shape[1] == 3:
-      return output_spatial_data
+        return output_spatial_data
 
 def calc_reward_intervals(timestamps, values):
     '''
