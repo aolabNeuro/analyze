08ed1e4afea1a977694db2f2b24998fb62bdba8e
Leo Scholl <leo.scholl@gmail.com>
2023-05-04 14:56:54 -0700

    add memory default

--- aopy/__init__.py
+++ aopy/__init__.py
@@ -1,2 +1,7 @@
 # __init__.py
+import socket
 from . import analysis, data, postproc, precondition, preproc, visualization, tutorial_functions, utils
+
+# Set the default memory limit
+if socket.gethostname() == 'crab-eating':
+    utils.set_memory_limit_gb(100)
