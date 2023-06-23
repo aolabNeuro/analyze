e020f950980ad5595bc88aa9ba3ccad132db6477
Leo Scholl <leo.scholl@gmail.com>
2023-06-23 10:45:42 -0700

    add class wrapper

--- aopy/data/db.py
+++ aopy/data/db.py
@@ -38,9 +38,9 @@ def lookup_sessions(subject=None, date=None, task_name=None, task_desc=None, ses
             kwargs['project'] = project
         if experimenter:
             kwargs['experimenter__name'] = experimenter
-        entries = bmi3d.get_task_entries(dbname=BMI3D_DBNAME, **kwargs)
+        entries = [TaskEntry(e) for e in bmi3d.get_task_entries(dbname=BMI3D_DBNAME, **kwargs)]
         if has_features:
-            filter_fn = filter_fn and (lambda x: all([has_feature(x, f) for f in has_features]))
+            filter_fn = filter_fn and (lambda x: all([x.has_feature(f) for f in has_features]))
         if len(entries) == 0:
             warnings.warn("No entries found")
             return []
@@ -63,122 +63,164 @@ def lookup_bmi_sessions(subject, date, session_name='training', bmi_task_name='b
 '''
 Paramters
 '''
-def get_subject(entry):
+class BMI3DTaskEntry():
     '''
-    Returns name of subject for session.
-    Takes TaskEntry object.
+    Wrapper class for bmi3d database entry classes. Written like this 
+    so that other database types can implement their own classes with
+    the same methods without needing to modfiy their database model.
     '''
-    return bmi3d.get_subject(entry, dbname=BMI3D_DBNAME)
 
-def get_id(entry):
-    '''
-    Returns id for session.
-    Takes TaskEntry object.
-    '''
-    return entry.id
+    def __init__(self, task_entry=None, dbname='default', **kwargs):
+        '''
+        '''
+        self.dbname = dbname
+        self.record = task_entry
 
-def get_time(entry):
-    '''
-    Returns date for session.
-    Takes TaskEntry object.
-    '''
-    return bmi3d.get_date(entry, dbname=BMI3D_DBNAME)
-
-def get_date(entry):
-    '''
-    Returns date for session.
-    Takes TaskEntry object.
-    '''
-    return bmi3d.get_date(entry, dbname=BMI3D_DBNAME).date()
-
-def get_features(entry):
-    return [f.name for f in entry.feats.all()]
-
-def has_feature(entry, featname):
-    features = get_features(entry)
-    return featname in features
-
-def get_params(entry):
-    '''
-    Returns a dict of all task params for session.
-    Takes TaskEntry object.
-    '''
-    return bmi3d.get_params(entry)
-
-def get_param(entry, paramname):
-    '''
-    Returns parameter value.
-    Takes TaskEntry object.
-    '''
-    params = bmi3d.get_params(entry)
-    if paramname not in params:
-        return None
-    return params[paramname]
-
-def get_task_name(entry):
-    '''
-    Returns name of task used for session.
-    Takes TaskEntry object.
-    '''
-    return bmi3d.get_task_name(entry, dbname=BMI3D_DBNAME)
-
-def get_notes(entry):
-    '''
-    Returns notes for session.
-    Takes TaskEntry object.
-    '''
-    return bmi3d.get_notes(entry)
-
-def get_length(entry):
-    '''
-    Returns length of session in seconds.
-    Takes TaskEntry object.
-    '''
-    return bmi3d.get_length(entry)
+    @property
+    def id(self):
+        return self.record.id
     
-def get_raw_files(entry, system_subfolders=None):
-    '''
-    Gets the raw data files associated with each task entry 
+    @property
+    def params(self):
+        return self.record.task_params
     
-    Args:
-        entry (TaskEntry): recording to find raw files for 
-        system_subfolders (dict, optional): dictionary of system subfolders where the 
-           data for that system is located. If None, defaults to the system name
+    @property
+    def time(self):
+        return self.record.date
     
-    Returns: 
-        files : list of (system, filepath) for each datafile associated with this task entry 
-    '''
-    return bmi3d.get_rawfiles_for_taskentry(entry, system_subfolders=system_subfolders)
+    @property
+    def date(self):
+        self.record.date.date()
+
+    @property
+    def notes(self):
+        return self.record.notes
+        
+    @property
+    def subject(self):
+        return self.record.subject.name
+    
+    @property
+    def decoder_record(self):
+        # Load decoder record
+        if 'decoder' in self.params:
+            self.decoder_record = models.Decoder.objects.using(self.record._state.db).get(pk=self.params['decoder'])
+        elif 'bmi' in self.params:
+            self.decoder_record = models.Decoder.objects.using(self.record._state.db).get(pk=self.params['bmi'])
+        else: # Try direct lookup
+            try:
+                self.decoder_record = models.Decoder.objects.using(self.record._state.db).get(entry_id=self.id)
+            except:
+                self.decoder_record = None
+
+        # Load the event log (report)
+        try:
+            self.report = json.loads(self.record.report)
+        except:
+            self.report = ''
+
+    @property
+    def features(self):
+        return [f.name for f in self.record.feats.all()]
+
+    def has_feature(self, featname):
+        return featname in self.features
+
+    @property
+    def params(self):
+        '''
+        Returns a dict of all task params for session.
+        Takes TaskEntry object.
+        '''
+        return self.record.task_params
+
+    @property
+    def sequence_params(self):
+        return self.record.sequence_params
+
+    def get_param(self, paramname):
+        '''
+        Returns parameter value.
+        Takes TaskEntry object.
+        '''
+        params = self.params
+        if paramname not in params:
+            return None
+        return params[paramname]
+    
+    def get_sequence_param(self, paramname):
+        '''
+        Returns parameter value.
+        Takes TaskEntry object.
+        '''
+        params = self.sequence_params
+        if paramname not in params:
+            return None
+        return params[paramname]
+
+    @property
+    def task_name(self):
+        '''
+        Returns name of task used for session.
+        Takes TaskEntry object.
+        '''
+        return bmi3d.get_task_name(self.record, dbname=self.dbname)
+
+    @property
+    def notes(self):
+        '''
+        Returns notes for session.
+        Takes TaskEntry object.
+        '''
+        return self.record.notes
+
+    @property
+    def length(self):
+        '''
+        Returns length of session in seconds.
+        Takes TaskEntry object.
+        '''
+        return bmi3d.get_length(self.record)
+        
+    @property
+    def raw_files(self, system_subfolders=None):
+        '''
+        Gets the raw data files associated with each task entry 
+        
+        Args:
+            entry (TaskEntry): recording to find raw files for 
+            system_subfolders (dict, optional): dictionary of system subfolders where the 
+            data for that system is located. If None, defaults to the system name
+        
+        Returns: 
+            files : list of (system, filepath) for each datafile associated with this task entry 
+        '''
+        return bmi3d.get_rawfiles_for_taskentry(self.record, system_subfolders=system_subfolders)
+
+    @property
+    def preprocessed_sources(self):
+        '''
+        Returns a list of datasource names that should be preprocessed for the given entry
+        '''
+        params = self.params
+        sources = ['exp']
+        if 'record_headstage' in params and params['record_headstage']:
+            sources.append('broadband')
+            sources.append('lfp')
+        if 'neuropixels' in self.features:
+            sources.append('spike')
+            sources.append('lfp')
+        sources.append('eye')
+        
+        return sources
 
 '''
 Wrappers
 '''
-def get_entry_details(entry):
-    '''
-    Returns subject, id, and date for the given entry
-    '''
-    subject = get_subject(entry)
-    te = get_subject(entry)
-    date = get_date(entry)
-    return subject, te, date
+def list_entry_details(entries):
+    return zip(*[(te.subject, te.id, te.date) for te in entries])
 
-def get_entries_details(entries):
-    return zip(*[get_entry_details(e) for e in entries])
-
-def get_preprocessed_sources(entry):
-    '''
-    Returns a list of datasource names that should be preprocessed for the given entry
-    '''
-    params = get_params(entry)
-    sources = ['exp']
-    if 'record_headstage' in params and params['record_headstage']:
-        sources.append('broadband')
-        sources.append('lfp')
-    sources.append('eye')
-    
-    return sources
-
-def group_entries(entries, grouping_fn=lambda te: get_date(te)):
+def group_entries(entries, grouping_fn=lambda te: te.date):
     '''
     Automatically group together a flat list of database IDs
 
