99d8843889ede70de8967d4061a5b7f138203043
resatomo <touchi@uw.edu>
2023-07-03 17:33:26 -0700

    change inter_barcode_interval

--- aopy/preproc/neuropixel.py
+++ aopy/preproc/neuropixel.py
@@ -1,7 +1,7 @@
 import numpy as np
 from ..utils import extract_barcodes_from_times, get_first_last_times, sync_timestamp_offline
 
-def sync_neuropixel_ecube(raw_timestamp,on_times_np,off_times_np,on_times_ecube,off_times_ecube,bar_duration=0.02,verbose=False):
+def sync_neuropixel_ecube(raw_timestamp,on_times_np,off_times_np,on_times_ecube,off_times_ecube,inter_barcode_interval=20,bar_duration=0.02,verbose=False):
     '''
     This function is specfic to synchronization between neuropixels and ecube.
     
@@ -22,8 +22,8 @@ def sync_neuropixel_ecube(raw_timestamp,on_times_np,off_times_np,on_times_ecube,
     
     while bar_duration >0.001:
         # Get each barcode timing and label
-        barcode_ontimes_np,barcode_np = extract_barcodes_from_times(on_times_np,off_times_np,bar_duration=bar_duration)
-        barcode_ontimes_ecube,barcode_ecube = extract_barcodes_from_times(on_times_ecube,off_times_ecube,bar_duration=bar_duration)
+        barcode_ontimes_np,barcode_np = extract_barcodes_from_times(on_times_np,off_times_np,inter_barcode_interval=inter_barcode_interval,bar_duration=bar_duration)
+        barcode_ontimes_ecube,barcode_ecube = extract_barcodes_from_times(on_times_ecube,off_times_ecube,inter_barcode_interval=inter_barcode_interval,bar_duration=bar_duration)
 
         # Check if barcodes are consistent across streams
         n_barcode = min(len(barcode_ecube),len(barcode_np))
