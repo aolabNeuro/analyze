3460bc39d27b1c373c6a0d3556022dadb140034e
Leo Scholl <leo.scholl@gmail.com>
2023-08-30 14:51:59 -0700

    update inits

--- aopy/utils/__init__.py
+++ aopy/utils/__init__.py
@@ -1,2 +1,6 @@
+# For legacy code, include these modules in the top-level analysis module
+# since they existed in base before becoming sub-modules
 from .base import *
 from .memory import *
+
+# For new submodules, import just the namespace
