1c02d5786d21f42d176dc95236e20c5c4e0b963f
TOMOHIRO OUCHI <touchi@uw.edu>
2023-12-07 14:23:42 -0800

    Added test

--- tests/test_postproc.py
+++ tests/test_postproc.py
@@ -247,6 +247,16 @@ class TestGetFuncs(unittest.TestCase):
         coefficients = np.array([[1, 1], [0, 0]]).T
         calibrated = get_calibrated_eye_data(eye_data, coefficients)
         np.testing.assert_array_equal(eye_data, calibrated)
+        
+    def test_get_conditioned_trials_per_target(self):
+        target_idx_test = np.array([1,2,3,1,2,3,1,1,3,2,3,1,2,1,3])
+        cond_mask_test = np.array([False,False,False,False,True,True,True,True,True,True,True,True,True,True,True])
+        min_trial = get_minimum_trials_per_target(target_idx_test, cond_mask_test)
+        trial_mask = get_conditioned_trials_per_target(target_idx_test, cond_mask_test, min_trial, replacement=False, seed=None)
+        self.assertEqual(min_trial,3)
+        self.assertTrue(sum(target_idx_test[trial_mask] == 1) == min_trial)
+        self.assertTrue(sum(target_idx_test[trial_mask] == 2) == min_trial)
+        self.assertTrue(sum(target_idx_test[trial_mask] == 3) == min_trial)
 
 class TestEyeFuncs(unittest.TestCase):
     
