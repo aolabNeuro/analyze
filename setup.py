0c0d4f5138cc2a5002b295bb75916d0c9c5d5d7c
Leo Scholl <leo.scholl@gmail.com>
2024-06-17 17:21:32 -0700

    bump version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ install_requires = [
 
 setuptools.setup(
     name="aolab-aopy",
-    version="0.7.2",
+    version="0.7.3",
     author="aoLab",
     author_email="aorsborn@uw.edu",
     description="python code repository for aoLab @UW",
