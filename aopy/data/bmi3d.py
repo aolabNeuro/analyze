e590f2793148d096f5263cb32f73a6d0cd6b06c3
Leo Scholl <leo.scholl@gmail.com>
2024-11-25 11:04:58 -0800

    typo

--- aopy/data/bmi3d.py
+++ aopy/data/bmi3d.py
@@ -645,7 +645,7 @@ def get_interp_task_data(exp_data, exp_metadata, datatype='cursor', samplerate=1
     if datatype == 'hand':
         warnings.warn("Raw hand position is not recommended for analysis. Use 'manual_input' instead for 3D world coordinate inputs.")
         data_cycles = exp_data['clean_hand_position'] # 3d hand position (e.g. raw optitrack coords: x,y,z) on each bmi3d cycle
-    if datatype == 'manual_input':
+    elif datatype == 'manual_input':
         # 3d input on each bmi3d cycle converted to world coordinates
         data_cycles = postproc.get_bmi3d_mc_input(exp_data['clean_hand_position'], exp_metadata['rotation'], 
                                                   exp_metadata['offset'], np.sign(exp_metadata['scale']))
