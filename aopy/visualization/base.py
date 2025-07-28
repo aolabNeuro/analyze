62821a80420abf3a076ec44bd7e1ba4eef9436af
Victoria Pierce <victoria.elaine.pierce@gmail.com>
2025-07-28 15:35:29 -0700

    If 3D data given with 2D axes, assume xy plane

--- aopy/visualization/base.py
+++ aopy/visualization/base.py
@@ -1163,6 +1163,7 @@ def plot_trajectories(trajectories, bounds=None, ax=None, **kwargs):
         ax (plt.Axis, optional): axis to plot the targets on
         kwargs (dict): keyword arguments to pass to the plt.plot function
    '''
+
     if ax is None:
         ax = plt.gca()
 
@@ -1181,17 +1182,19 @@ def plot_trajectories(trajectories, bounds=None, ax=None, **kwargs):
         if stacked.shape[1]>2:
 
             if not zero_cols.any():
-                raise ValueError("Axis is unclear for 3D data (no zero columns). Cannot plot in 2D.")
+                warnings.warn("Axis is unclear for 3D data (no zero columns). Plots assume data of interest is in xy-plane.", stacklevel=2)
+                zero_col_idx = [2] # assume z-axis data is unwanted
+                col_to_remove = zero_col_idx[0] # tag z-axis data for removal
             else:
                 zero_col_idx = np.where(zero_cols)[0]
                 
-                if len(zero_col_idx) > 1: # 1D data; remove 2nd all-zero axis
-                    col_to_remove = zero_col_idx[1]
-                else:
-                    col_to_remove = zero_col_idx[0] # 2D data; remove only all-zero axis
+                if len(zero_col_idx) > 1: # 1D data
+                    col_to_remove = zero_col_idx[1] # tag the 2nd all-zero axis for removal
+                else: # 2D data
+                    col_to_remove = zero_col_idx[0] # tag the only all-zero axis for removal
                     
-                flattened = [traj[:, [i for i in range(3) if i != col_to_remove]] for traj in trajectories]
-                flattened = np.array(flattened, dtype=object)
+            flattened = [traj[:, [i for i in range(3) if i != col_to_remove]] for traj in trajectories]
+            flattened = np.array(flattened, dtype=object)
 
             if np.all(np.isin(2, zero_col_idx)):
                 ax.set_xlabel('x')
@@ -1372,7 +1375,9 @@ def gradient_trajectories(trajectories, n_colors=100, color_palette='viridis', b
         if stacked.shape[1]>2:
 
             if not zero_cols.any():
-                raise ValueError("Axis is unclear for 3D data (no zero columns). Cannot plot in 2D.")
+                warnings.warn("Axis is unclear for 3D data (no zero columns). Plots assume data of interest is in xy-plane.", stacklevel=2)
+                zero_col_idx = [2] # assume z-axis data is unwanted
+                col_to_remove = zero_col_idx[0] # tag z-axis data for removal
             else:
                 zero_col_idx = np.where(zero_cols)[0]
                 
@@ -1381,8 +1386,8 @@ def gradient_trajectories(trajectories, n_colors=100, color_palette='viridis', b
                 else:
                     col_to_remove = zero_col_idx[0] # 2D data; remove only all-zero axis
                     
-                flattened = [traj[:, [i for i in range(3) if i != col_to_remove]] for traj in trajectories]
-                flattened = np.array(flattened, dtype=object)
+            flattened = [traj[:, [i for i in range(3) if i != col_to_remove]] for traj in trajectories]
+            flattened = np.array(flattened, dtype=object)
 
             if np.all(np.isin(2, zero_col_idx)):
                 ax.set_xlabel('x')
@@ -1422,11 +1427,12 @@ def gradient_trajectories(trajectories, n_colors=100, color_palette='viridis', b
             ax.add_collection(lc)
             
         ax.set_aspect('equal', adjustable='box')
+        
 
     if bounds is not None: 
         set_bounds(bounds, ax)
     else:
-        ax.margins(0.05) # The ax.add_collection() call doesn't automatically set margins        
+        ax.margins(0.05) # The ax.add_collection() call doesn't automatically set margins
 
 def plot_sessions_by_date(trials, dates, *columns, method='sum', labels=None, ax=None):
     '''
